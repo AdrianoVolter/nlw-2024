bc22d7906d350a2141e8c12a9a2c8ad4a17621cd
adrianovolter <adriano.j.volter@hotmail.com>
2024-07-09 12:30:35 -0300

    criado envio de email com lib nodemailer

--- Backend/src/routes/create-trip.ts
+++ Backend/src/routes/create-trip.ts
@@ -2,7 +2,9 @@ import type { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import dayjs from "dayjs";
 import { z } from "zod";
-import  { prisma }  from "../lib/prisma";
+import { prisma } from "../lib/prisma";
+import { getMailClient } from "../lib/mail";
+import nodemailer from "nodemailer";
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -13,17 +15,20 @@ export async function createTrip(app: FastifyInstance) {
           destination: z.string().min(4),
           start_at: z.coerce.date(),
           end_at: z.coerce.date(),
+          owner_name: z.string().min(4),
+          owner_email: z.string().email(),
         }),
       },
     },
     async (request) => {
-      const { destination, start_at, end_at } = request.body;   
+      const { destination, start_at, end_at, owner_name, owner_email } =
+        request.body;
 
-      if(dayjs(start_at).isBefore(new Date())) {
+      if (dayjs(start_at).isBefore(new Date())) {
         throw new Error("Invalid start trip date");
       }
 
-      if(dayjs(end_at).isBefore(dayjs(start_at))) {
+      if (dayjs(end_at).isBefore(dayjs(start_at))) {
         throw new Error("Invalid trip end date");
       }
 
@@ -34,6 +39,23 @@ export async function createTrip(app: FastifyInstance) {
           end_at,
         },
       });
+
+      const mail = await getMailClient();
+
+    const message =  await mail.sendMail({
+        from: {
+            name: "Trip Planner",
+            address: "noreply@tripplanner.com"
+        },
+        to: {
+            name: owner_name,
+            address: owner_email
+        },
+        subject: "Trip created successfully! 🎉 - email de test",
+        html: "<p>Hello, this is a test email from Trip Planner</p>",
+      });
+
+      console.log(nodemailer.getTestMessageUrl(message));
       return {
         statusCode: 200,
         body: trip,
