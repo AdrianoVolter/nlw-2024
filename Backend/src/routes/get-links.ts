f01bd6841879e1a7b2a9c73ba49367b1081bc930
adrianovolter <adriano.j.volter@hotmail.com>
2024-07-10 14:51:01 -0300

    Adiciona rota que busca um link

---
+++ Backend/src/routes/get-links.ts
@@ -0,0 +1,37 @@
+import type { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+import { dayjs } from "../lib/dayjs";
+
+export async function getLinks(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/trips/:tripId/links",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+      },
+    },
+    async (request) => {
+      const { tripId } = request.params;
+
+      const trip = await prisma.trip.findUnique({
+        where: {
+          id: tripId,
+        },
+        include: {
+            links: true,
+          
+        },
+      });
+
+      if (!trip) {
+        throw new Error("Trip not found");
+      }
+
+      return { links: trip.links};
+    }
+  );
+}
--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -10,6 +10,7 @@ import { confirmParticipant } from "./routes/confirm-participant";
 import { createActivity } from "./routes/create-activity";
 import { getActivity } from "./routes/get-activities";
 import { createLink } from "./routes/create-link";
+import { getLinks } from "./routes/get-links";
 
 const app = fastify();
 
@@ -26,6 +27,7 @@ app.register(confirmParticipant);
 app.register(createActivity);
 app.register(getActivity);
 app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server started at http://localhost:3333");
